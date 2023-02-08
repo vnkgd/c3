bb007a6cc7e3df367725c67e5cd1f0f95af40034
vnkgd <vnkgd@mail.ru>
2023-02-08 21:20:40 +0200

    Коммент8

---
+++ Example_005_Random/Program.cs
@@ -0,0 +1,7 @@
+﻿// Sum
+int numberA = new Random().Next(1, 10);//от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);//от 1 до 9
+int result = numberA + numberB;
+Console.WriteLine(numberB);
+Console.WriteLine(result);
