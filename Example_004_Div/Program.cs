a3329013d27a4cb1c28ddb5758ddddc109e781cb
vnkgd <vnkgd@mail.ru>
2023-02-08 18:26:46 +0200

    Коммент7

--- Example_004_Div/Program.cs
+++ Example_004_Div/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Sum
+double numberA = 12;
+double numberB = 5;
+//int result = numberA + numberB;
+Console.WriteLine(numberA / numberB);
