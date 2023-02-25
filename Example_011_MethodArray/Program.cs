265caa1b552fc81470e053a02e6b703c8a6e000a
vnkgd <vnkgd@mail.ru>
2023-02-25 21:05:46 +0200

    Коммент20

--- Example_011_MethodArray/Program.cs
+++ Example_011_MethodArray/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index+1;
+    index++;
+}
