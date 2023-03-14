337f9d50eb85839e6a82c6180e96f50676dcc93a
vnkgd <vnkgd@mail.ru>
2023-03-14 18:20:28 +0200

    Коммент24

---
+++ Examle_015_CodeStyle/Program.cs
@@ -0,0 +1,13 @@
+﻿// Красивый код
+
+using System.Linq; // используем системные библиотеки.
+
+//int a = 12; //даже если написать var вместо int, всё равно покажет Int32. заранее записывать не надо.
+//Console.WriteLine(a.GetType()); //узнаём тип данных. в ответ пришло System.Int32 c .Name возвращает только Int32
+
+var data = new int[] {1,2,3,4}
+            .Where(e=> e>0)
+            .Select(e=> new{q = e, w = e + 1});
+Console.WriteLine(data.GetType().Name);    
+
+//a = 123; // так делать не надо
